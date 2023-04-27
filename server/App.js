606a7198163677a8c45c3b85151d5bfabdf03032
Patrick Borgella <patrickborgella1@gmail.com>
2023-04-27 15:16:17 -0500

    app moved back into the server file

--- server/App.js
+++ server/App.js
@@ -23,15 +23,4 @@ app.use((error, req, res, next) => {
     res.send({ error: error.message, name: error.name, message: error.message });
 });
 
-
-// module.exports = { app };
-const { PORT = 3000 } = process.env;
-
-
-app.listen(PORT, () => {
-  try {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-  } catch (error) {
-    console.error("Error starting server:", error);
-  }
-});
+module.exports = { app }
--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
 const { app } = require("./App");
 
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  try {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+});
