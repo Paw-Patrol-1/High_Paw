b6049ad8b7c6dbf73fe9a663d69a6d65eef0b8ce
Kharmalina Tong <kharmalinat7@gmail.com>
2023-05-04 14:20:06 -0400

    handle JWT errors, add refreshToken func and route, further accessToken implementation to register and login routes

--- server/helpers/jwt_helper.js
+++ server/helpers/jwt_helper.js
@@ -1,14 +1,14 @@
 const JWT = require('jsonwebtoken');
 const createError = require('http-errors');
 require("dotenv").config();
-const { ACCESS_TOKEN_SECRET } = process.env;
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
 const signAccessToken = (userId) => {
     return new Promise((resolve, reject) => {
         const payload = {}
         const secret = ACCESS_TOKEN_SECRET
         const options = {
-            expiresIn: "1w",
+            expiresIn: "20s",
             audience: userId
         }
         JWT.sign(payload, secret, options, (err, token) => {
@@ -21,6 +21,51 @@ const signAccessToken = (userId) => {
     })
 }
 
+// middleware to protect routes
+const verifyAccessToken = (req, res, next) => {
+   if (!req.header("Authorization")) return next(createError.Unauthorized())
+   const authHeader = req.header("Authorization")
+   const splitToken = authHeader.split(" ");
+   const token = splitToken[1];
+   JWT.verify(token, ACCESS_TOKEN_SECRET, (err, payload) => {
+    if(err) {
+        const message = err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message
+        return next(createError.Unauthorized(message))
+    }
+    req.payload = payload
+    next()
+   })
+}
+
+const signRefreshToken = (userId) => {
+    return new Promise((resolve, reject) => {
+        const payload = {}
+        const secret = REFRESH_TOKEN_SECRET
+        const options = {
+            expiresIn: "1y",
+            audience: userId
+        }
+        JWT.sign(payload, secret, options, (err, token) => {
+            if (err) {
+                console.log(err.message)
+                return reject(createError.InternalServerError())
+            }
+            resolve(token)
+        })
+    })
+}
+
+const verifyRefreshToken = (refreshToken) => {
+    return new Promise((resolve, reject) => {
+        JWT.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, payload) => {
+            if (err) return reject(createError.Unauthorized())
+            const userId = payload.aud
+
+            resolve(userId)
+        })
+    })
+}
+
 module.exports = {
-    signAccessToken
+    signAccessToken, verifyAccessToken, signRefreshToken, verifyRefreshToken
 }
--- server/routes/Auth.route.js
+++ server/routes/Auth.route.js
@@ -4,10 +4,24 @@ const User = require("../Models/User.model");
 const {auth} = require("../config/auth.config");
 const createError = require('http-errors');
 const {authSchema, loginSchema} = require(`../helpers/validation_schema`);
-const {signAccessToken} = require('../helpers/jwt_helper');
+const {signAccessToken, verifyAccessToken, signRefreshToken, verifyRefreshToken} = require('../helpers/jwt_helper');
 
-router.get('/', async (req, res, next) => {
+router.get('/', verifyAccessToken, async (req, res, next) => {
+    console.log(req.header("Authorization"));
     res.status(200).send("Welcome to the login, register and logout APIs");
+})
+
+router.post("/refresh-token", async (req, res, next) => {
+    try {
+        const { refreshToken } = req.body;
+        if (!refreshToken) throw createError.BadRequest();
+        const userId = await verifyRefreshToken(refreshToken);
+        const accessToken = await signAccessToken(userId);
+        const refToken = await signRefreshToken(userId);
+        res.send({ accessToken: accessToken, refreshToken: refToken })
+    } catch (error) {
+        next(error)
+    }
 })
 
 router.post('/register', async (req, res, next) => {
@@ -15,17 +29,16 @@ router.post('/register', async (req, res, next) => {
         const result = await authSchema.validateAsync(req.body);
         console.log(result);
 
-        if (result.password!=result.confirmPassword){
-            res.status(400).send({message:"password does not match"})
-        }
+        // if (result.password!=result.confirmPassword) return res.status(400).send({message:"password does not match"})
 
         const doesExist = await User.findOne({email: result.email});
         if (doesExist) throw createError.Conflict(`${result.email} is already regsistered`);
 
         const user = new User(result)
         const savedUser = await user.save();
-        const accessToken = await signAccessToken(savedUser.id)
-        res.send({ accessToken })
+        const accessToken = await signAccessToken(savedUser.id);
+        const refreshToken = await signRefreshToken(savedUser.id);
+        res.send({ accessToken, refreshToken })
     } catch (error) {
         if(error.isJoi === true) res.status(422)
         next(error);
@@ -43,8 +56,10 @@ router.post('/login', async (req,res, next) => {
 
         if(!isMatch) throw createError.Unauthorized('Username or Password is not valid');
 
-        const accessToken = await signAccessToken(user.id)
-        res.send({ accessToken })
+        const accessToken = await signAccessToken(user.id);
+        const refreshToken = await signRefreshToken(user.id);
+
+        res.send({ accessToken, refreshToken })
     } catch (error) {
         if (error.isJoi === true) return next(createError.BadRequest("Invalid username or Password"));
         next(error)
