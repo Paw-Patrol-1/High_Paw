4ea3a89c1214b3ed9ea4b0cf252e6584a01a7d89
Patrick Borgella <patrickborgella1@gmail.com>
2023-05-06 11:16:13 -0500

    Creates connection to redis to cache refresh tokens, creates controller file for auth route functions

--- server/helpers/jwt_helper.js
+++ server/helpers/jwt_helper.js
@@ -1,71 +1,91 @@
-const JWT = require('jsonwebtoken');
-const createError = require('http-errors');
+const JWT = require("jsonwebtoken");
+const createError = require("http-errors");
 require("dotenv").config();
 const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
+const client = require("./init_redis");
 
 const signAccessToken = (userId) => {
-    return new Promise((resolve, reject) => {
-        const payload = {}
-        const secret = ACCESS_TOKEN_SECRET
-        const options = {
-            expiresIn: "20s",
-            audience: userId
-        }
-        JWT.sign(payload, secret, options, (err, token) => {
-            if (err) {
-                console.log(err.message)
-                return reject(createError.InternalServerError())
-            }
-            resolve(token)
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const payload = {};
+    const secret = ACCESS_TOKEN_SECRET;
+    const options = {
+      expiresIn: "5m",
+      audience: userId,
+    };
+    JWT.sign(payload, secret, options, (err, token) => {
+      if (err) {
+        console.log(err.message);
+        return reject(createError.InternalServerError());
+      }
+      resolve(token);
+    });
+  });
+};
 
 // middleware to protect routes
 const verifyAccessToken = (req, res, next) => {
-   if (!req.header("Authorization")) return next(createError.Unauthorized())
-   const authHeader = req.header("Authorization")
-   const splitToken = authHeader.split(" ");
-   const token = splitToken[1];
-   JWT.verify(token, ACCESS_TOKEN_SECRET, (err, payload) => {
-    if(err) {
-        const message = err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message
-        return next(createError.Unauthorized(message))
+  if (!req.header("Authorization")) return next(createError.Unauthorized());
+  const authHeader = req.header("Authorization");
+  const splitToken = authHeader.split(" ");
+  const token = splitToken[1];
+  JWT.verify(token, ACCESS_TOKEN_SECRET, (err, payload) => {
+    if (err) {
+      const message =
+        err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+      return next(createError.Unauthorized(message));
     }
-    req.payload = payload
-    next()
-   })
-}
+    req.payload = payload;
+    next();
+  });
+};
 
 const signRefreshToken = (userId) => {
-    return new Promise((resolve, reject) => {
-        const payload = {}
-        const secret = REFRESH_TOKEN_SECRET
-        const options = {
-            expiresIn: "1y",
-            audience: userId
+  return new Promise((resolve, reject) => {
+    const payload = {};
+    const secret = REFRESH_TOKEN_SECRET;
+    const options = {
+      expiresIn: "1y",
+      audience: userId,
+    };
+    JWT.sign(payload, secret, options, (err, token) => {
+      if (err) {
+        console.log(err.message);
+        reject(createError.InternalServerError());
+      }
+
+      client.SET(userId, token, "EX", 365 * 24 * 60 * 60, (err, reply) => {
+        if (err) {
+          console.log(err.message);
+          reject(createError.InternalServerError());
+          return;
         }
-        JWT.sign(payload, secret, options, (err, token) => {
-            if (err) {
-                console.log(err.message)
-                return reject(createError.InternalServerError())
-            }
-            resolve(token)
-        })
-    })
-}
+        resolve(token);
+      });
+    });
+  });
+};
 
 const verifyRefreshToken = (refreshToken) => {
-    return new Promise((resolve, reject) => {
-        JWT.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, payload) => {
-            if (err) return reject(createError.Unauthorized())
-            const userId = payload.aud
-
-            resolve(userId)
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    JWT.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, payload) => {
+      if (err) return reject(createError.Unauthorized());
+      const userId = payload.aud;
+      client.GET(userId, (err, result) => {
+        if (err) {
+          console.log(err.message);
+          reject(createError.InternalServerError());
+          return;
+        }
+        if (refreshToken === result) return resolve(userId);
+        reject(createError.Unauthorized());
+      });
+    });
+  });
+};
 
 module.exports = {
-    signAccessToken, verifyAccessToken, signRefreshToken, verifyRefreshToken
-}
+  signAccessToken,
+  verifyAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+};
