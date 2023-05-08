88600558275bf62858506ee93dfab8d068468947
Patrick Borgella <patrickborgella1@gmail.com>
2023-05-07 19:07:24 -0500

    Completes Hangout route and Hangout controller

--- server/controllers/Hangout.Controller.js
+++ server/controllers/Hangout.Controller.js
@@ -1,19 +1,19 @@
 const Hangout = require("../Models/Hangout.model");
 const createError = require("http-errors");
 const { hangoutSchema } = require("../helpers/validation_schema");
+const { all } = require("../routes/Auth.route");
 
 module.exports = {
   newHangout: async (req, res, next) => {
     try {
       const result = await hangoutSchema.validateAsync(req.body);
-      console.log(result);
 
       const doesExist = await Hangout.findOne({ title: result.title });
-      //   if (doesExist)
-      // throw createError.Conflict(`${result.title} is already taken`);
+      if (doesExist)
+        throw createError.Conflict(`${result.title} is already taken`);
 
-      //   const hangout = new Hangout(result);
-      //   const savedHangout = await hangout.save();
+      const hangout = new Hangout(result);
+      const savedHangout = await hangout.save();
 
       const hangouts = await Hangout.find();
 
@@ -31,14 +31,32 @@ module.exports = {
       next(error);
     }
   },
+  singleHangout: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const hangout = await Hangout.findById(id);
+
+      if (!hangout) {
+        throw createError.Conflict("Hangout id is incorrect or does not exist");
+      }
+
+      res.send({ hangout });
+    } catch (error) {
+      next(error);
+    }
+  },
   deleteHangout: async (req, res, next) => {
     try {
       const { id } = req.params;
-      console.log(id);
-      const hangout = await Hangout.findById(id);
-      console.log(hangout);
 
-      res.sendStatus(200);
+      const hangout = await Hangout.findById(id);
+      if (!hangout) {
+        throw createError.Conflict("Hangout id is incorrect or does not exist");
+      }
+
+      await Hangout.deleteOne({ _id: id });
+
+      res.status(200).send({ message: "Hangout was deleted" });
     } catch (error) {
       next(error);
     }
--- server/routes/Hangout.route.js
+++ server/routes/Hangout.route.js
@@ -7,10 +7,10 @@ const HangoutController = require("../controllers/Hangout.Controller");
 
 router.get("/all", verifyAccessToken, HangoutController.allHangouts);
 
-router.get("/:id", HangoutController.allHangouts);
+router.get("/:id", verifyAccessToken, HangoutController.singleHangout);
 
-router.post("/create", HangoutController.newHangout);
+router.post("/create", verifyAccessToken, HangoutController.newHangout);
 
-router.delete("/:id", HangoutController.deleteHangout);
+router.delete("/:id", verifyAccessToken, HangoutController.deleteHangout);
 
 module.exports = router;
