63ec15fc28ede474c5cf9eca3ca678104c2ae0a0
Adrian Baltag <fbaltag83@gmail.com>
2023-05-23 21:38:53 -0400

    touchup navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 function Navbar() {
+  const [clickedLink, setClickedLink] = useState("");
   const [showLinks, setShowLinks] = useState(false);
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
@@ -33,33 +34,65 @@ function Navbar() {
   const toggleNavbar = () => {
     setShowLinks(!showLinks);
   };
+  //if a link is clicked, set bg color to green
+  const handleClick = (link) => {
+    setClickedLink(link);
+  };
 
   return (
-    <div className=" flex flex-row  w-full justify-between border border-green-700">
+    <div className=" flex flex-row  w-full justify-between shadow-green-500 shadow-lg">
       {/* set dimension to div holding the svg */}
-      <div className="logo " style={{ height: "40px", width: "40px" }}>
+      <div className="logo" style={{ height: "40px", width: "40px" }}>
         {/* give 100% w / h for the svg to fill its parent */}
-        <Link to="/">
+
+        <Link to="/" className="animate-pulse ">
           {" "}
           <Logo style={{ width: "100%", height: "100%" }} />
         </Link>
       </div>
+
       <div
-        className={`links gap-5  text-stone-600  border border-red-500 absolute ${
+        className={`links gap-5  text-stone-600   absolute ${
           showLinks ? "top-10" : "-top-72"
-        } right-0 flex flex-col bg-white md:static md:flex-row md:mx-auto transition-all duration-500`}
+        } right-0 flex flex-col bg-white md:static text-lg px-4  md:flex-row md:mx-auto transition-all duration-500`}
       >
-        <Link to="/profile" className="">
+        <Link
+          onClick={() => handleClick("profile")}
+          to="/profile"
+          className={`text-stone-700 hover:font-semibold transition duration-300 hover:bg-green-400 my-2 mx-2 px-2 rounded-lg hover:text-white  hover:shadow-xl ${
+            clickedLink === "profile" ? "bg-green-500 text-white shadow-xl" : ""
+          }`}
+        >
           profile
         </Link>
-        <Link to="/hangouts">hangouts</Link>
+        <Link
+          onClick={() => handleClick("hangouts")}
+          to="/hangouts"
+          className={`text-stone-700 hover:font-semibold transition duration-300 hover:bg-green-400 my-2 mx-2 px-2 rounded-lg hover:text-white  hover:shadow-xl ${
+            clickedLink === "hangouts"
+              ? "bg-green-500 text-white shadow-xl"
+              : ""
+          }`}
+        >
+          hangouts
+        </Link>
 
-        <Link to="/create_hangout">create hangout</Link>
+        <Link
+          onClick={() => handleClick("create_hangout")}
+          to="/create_hangout"
+          className={`text-stone-700 hover:font-semibold transition duration-300 hover:bg-green-400 my-2 mx-2 px-2 rounded-lg hover:text-white  hover:shadow-xl ${
+            clickedLink === "create_hangout"
+              ? "bg-green-500 text-white shadow-xl"
+              : ""
+          }`}
+        >
+          create hangout
+        </Link>
 
         <div className="logoutBtn mr-3 md:absolute md:right-0">
           {user ? (
             <button
-              className=" fill-stone-800 outline outline-offset-2 rounded-full mt-2   outline-red-900 px-4 shadow-2xl hover:bg-red-950 hover:text-white transition-all"
+              className=" fill-stone-800 outline outline-offset-2 rounded-full mt-2   outline-red-700 px-4 shadow-2xl hover:bg-red-900 hover:text-white transition-all text-sm"
               onClick={handleLogout}
             >
               Logout
@@ -82,7 +115,10 @@ function Navbar() {
           )}
         </div>
       </div>
-      <button className="md:hidden" onClick={toggleNavbar}>
+      <button
+        className="md:hidden  text-2xl text-green-500"
+        onClick={toggleNavbar}
+      >
         <RxHamburgerMenu />
       </button>
     </div>
